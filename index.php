250d4d1d804924a0aec908979363bd9055bf3837
Mostafa Rhazlani <mo.rhazlani@gmail.com>
2025-02-09 17:30:48 +0100

    Implement role-based page access control for Admin, Artist, and User

--- index.php
+++ index.php
@@ -4,17 +4,47 @@
     require_once 'autoload.php';
     $home = new HomeController;
     
-    $pages = ['home', 'add', 'update', 'delete', 'auth/login', 'auth/register', 'artist/songs', 'artist/dashboard'];
+    $pagesAdmin = ['auth/login', 'auth/register', 'admin/category'];
+    $pagesArtist = ['albums', 'auth/login', 'auth/register', 'artist/songs', 'artist/dashboard'];
+    $pagesUser = ['home', 'albums', 'auth/login', 'auth/register', 'artist/songs', 'artist/dashboard'];
 
     if(isset($_GET['page'])) {
-        if(in_array($_GET['page'], $pages)) {
-            $page = $_GET['page'];
-            if($page !== 'auth/login' && $page !== 'auth/register') {
-                require_once 'views/layout/sidebar.php';
+        if(isset($_SESSION['user'])) {
+            if($_SESSION['user']['role'] == 'Admin') {
+                if(in_array($_GET['page'], $pagesAdmin)) {
+                    $page = $_GET['page'];
+                    if($page !== 'auth/login' && $page !== 'auth/register') {
+                        require_once 'views/layout/sidebar.php';
+                    }
+                    $home->index($page);
+                } else {
+                    include 'views/include/error404.php';
+                }
+            } else if($_SESSION['user']['role'] == 'Artist') {
+                if(in_array($_GET['page'], $pagesArtist)) {
+                    $page = $_GET['page'];
+                    if($page !== 'auth/login' && $page !== 'auth/register') {
+                        require_once 'views/layout/sidebar.php';
+                    }
+                    $home->index($page);
+                } else {
+                    include 'views/include/error404.php';
+                }
+            } else if($_SESSION['user']['role'] == 'User') {
+                if(in_array($_GET['page'], $pagesUser)) {
+                    $page = $_GET['page'];
+                    if($page !== 'auth/login' && $page !== 'auth/register') {
+                        require_once 'views/layout/sidebar.php';
+                    }
+                    $home->index($page);
+                } else {
+                    include 'views/include/error404.php';
+                }
+            } else {
+                $home->index('auth/login');
             }
-            $home->index($page);
         } else {
-            include 'views/include/error404.php';
+            $home->index('auth/login');
         }
     } else {
         $home->index('home');
